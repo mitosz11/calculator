51a897d7e8272f6d24a28ccbc9bcebc903a04842
Bence <bsalgoi@gmail.com>
2024-09-11 22:58:57 +0200

    display component

---
+++ src/components/Display.jsx
@@ -0,0 +1,9 @@
+const Display = ({ value }) => {
+  return (
+    <div className="bg-gray-900 text-white p-4 rounded-lg text-right text-3xl mb-2 border border-gray-600 shadow-lg">
+      {value}
+    </div>
+  );
+};
+
+export default Display;
